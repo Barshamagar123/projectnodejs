935d3e863349938c78512fc4004fb0a896b335cb
Barsha Magar <barshamagar@Barshas-MacBook-Air.local>
2025-06-24 21:28:33 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express=require("express")
+const app=express()
+app.set("views engine","ejs")
+app.get("/",(request,response)=>{
+    response.render("home.ejs")
+})
+app.get("/register",(request,response)=>{
+    response.render("./authentication/register.ejs")
+})
+app.get("/login",(request,response)=>{
+    response.render("./authentication/login.ejs")
+})
+app.get("/dashboard",(request,response)=>{
+    response.render("./studentdashboard/layout.ejs")
+})
+app.get("/students",(request,response)=>{
+    response.render("./admindashboard/student/display.ejs")
+})
+app.get("/departments",(request,response)=>{
+    response.render("./admindashboard/department/display-department.ejs")
+})
+app.get("/addDepartment",(request,response)=>{
+    response.render("./admindashboard/department/addDepartment.ejs")
+})
+app.get("/course",(request,response)=>{
+    response.render("./admindashboard/course/courses.ejs")
+})
+app.get("/results",(request,response)=>{
+    response.render("./admindashboard/result/results.ejs")
+})
+app.get("/addStudent",(request,response)=>{
+    response.render("./admindashboard/student/addStudent.ejs")
+})
+app.get("/admin",(request,response)=>{
+    response.render("./admindashboard/layout.ejs")
+})
+app.get("/team",(request,response)=>{
+    response.render("./admindashboard/team/displayTeam.ejs")
+})
+app.get("/addTeam",(request,response)=>{
+    response.render("./admindashboard/team/addTeam.ejs")
+})
+app.get("/addCourse",(request,response)=>{
+    response.render("./admindashboard/course/addCourse.ejs")
+})
+app.get("/importresults",(request,response)=>{
+    response.render("./admindashboard/result/importresults.ejs")
+})
+app.get("/events",(request,response)=>{
+    response.render("./admindashboard/event/displayEvent.ejs")
+})
+app.get("/addEvents",(request,response)=>{
+    response.render("./admindashboard/event/addEvent.ejs")
+})
+app.get("/addnotice",(request,response)=>{
+    response.render("./admindashboard/notice/addnotice.ejs")
+})
+app.get("/notice",(request,response)=>{
+    response.render("./admindashboard/notice/displaynotice.ejs")
+})
+app.listen(4000,(request,respone)=>{
+    console.log("backend has started at port number 4000")
+})
---
+++ database/db.js
@@ -0,0 +1,5 @@
+const {Sequelize,DataTypes}=require("sequelize")
+    require("dotenv").config()
+const sequelize=new Sequelize({
+    
+})
